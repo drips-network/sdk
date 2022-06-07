0931ce3e9df1dbf2b7a230e29fe45374daea4ade
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2022-06-07 17:55:22 +0200

    style: add linting and fix errors
    Resolves: https://github.com/radicle-dev/drips-js-sdk/issues/22

--- src/dripsclient.ts
+++ src/dripsclient.ts
@@ -1,136 +1,152 @@
-import { BigNumberish, providers, Signer, utils } from "ethers";
-import type { Dai, DaiDripsHub, RadicleRegistry } from '../contracts';
-import { getContractsForNetwork } from './contracts'
-import { constants } from 'ethers';
+import { BigNumberish, providers, Signer, utils, constants } from 'ethers';
+import { DripsReceiverStruct, SplitsReceiverStruct } from 'contracts/DaiDripsHub';
+import type { Dai, DaiDripsHub } from '../contracts';
+import { getContractsForNetwork } from './contracts';
 import { validateDrips, validateSplits } from './utils';
-import { Dai__factory } from "../contracts/factories/Dai__factory";
-import { DaiDripsHub__factory } from "../contracts/factories/DaiDripsHub__factory";
-import { RadicleRegistry__factory } from "../contracts/factories/RadicleRegistry__factory";
+import { Dai__factory } from '../contracts/factories/Dai__factory';
+import { DaiDripsHub__factory } from '../contracts/factories/DaiDripsHub__factory';
 
-export class DripsClient {
-  provider: providers.Web3Provider
-  signer: Signer
-  address: string
-  networkId: number
+export default class DripsClient {
+	provider: providers.Web3Provider;
+	signer: Signer;
+	address: string;
+	networkId: number;
 
-  contractDetails: any
+	contractDetails: any;
 
-  daiContract: Dai
-  hubContract: DaiDripsHub;
+	daiContract: Dai;
+	hubContract: DaiDripsHub;
 
-  constructor(provider: providers.Web3Provider, networkName: string) {
-    this.provider = provider
-    this.contractDetails = getContractsForNetwork(networkName)
+	constructor(provider: providers.Web3Provider, networkName: string) {
+		this.provider = provider;
+		this.contractDetails = getContractsForNetwork(networkName);
 
-    this.daiContract = Dai__factory.connect(this.contractDetails.CONTRACT_DAI, this.provider)
-    this.hubContract = DaiDripsHub__factory.connect(this.contractDetails.CONTRACT_DRIPS_HUB, this.provider)
-  }
+		this.daiContract = Dai__factory.connect(this.contractDetails.CONTRACT_DAI, this.provider);
+		this.hubContract = DaiDripsHub__factory.connect(this.contractDetails.CONTRACT_DRIPS_HUB, this.provider);
+	}
 
-  async connect() {
-    try {
-      this.signer = this.provider.getSigner()
-      const signerAddress = await this.signer.getAddress()
-      this.signIn(signerAddress)
-      this.networkId = (await this.provider.getNetwork()).chainId
-      return true
-    } catch (e) {
-      this.disconnect()
-      throw e
-    }
-  }
+	async connect() {
+		try {
+			this.signer = this.provider.getSigner();
+			const signerAddress = await this.signer.getAddress();
+			this.signIn(signerAddress);
+			this.networkId = (await this.provider.getNetwork()).chainId;
+			return true;
+		} catch (e) {
+			this.disconnect();
+			throw e;
+		}
+	}
 
-  disconnect () {
-    this.signOut()
-    this.signer = null
-    this.networkId = null;
-  }
+	disconnect() {
+		this.signOut();
+		this.signer = null;
+		this.networkId = null;
+	}
 
-  get connected () : boolean {
-    return !!this.networkId;
-  }
+	get connected(): boolean {
+		return !!this.networkId;
+	}
 
-  private signIn (signInAddress: string) {
-    this.address = signInAddress.toLowerCase()
-  }
+	private signIn(signInAddress: string) {
+		this.address = signInAddress.toLowerCase();
+	}
 
-  private signOut () {
-    this.address = null
-  }
+	private signOut() {
+		this.address = null;
+	}
 
-  approveDAIContract () {
-    if (!this.signer) throw new Error('DripsClient must be connected before approving DAI')
+	approveDAIContract() {
+		if (!this.signer) throw new Error('DripsClient must be connected before approving DAI');
 
-    const contractSigner = this.daiContract.connect(this.signer)
-    return contractSigner.approve(this.contractDetails.CONTRACT_DRIPS_HUB, constants.MaxUint256)
-  }
+		const contractSigner = this.daiContract.connect(this.signer);
+		return contractSigner.approve(this.contractDetails.CONTRACT_DRIPS_HUB, constants.MaxUint256);
+	}
 
-  updateUserDrips: DaiDripsHub['setDrips(uint64,uint128,(address,uint128)[],int128,(address,uint128)[])'] =
-    (lastUpdate, lastBalance, currentReceivers, balanceDelta, newReceivers) => {
-      if (!this.signer) throw new Error("Not connected to wallet")
+	updateUserDrips(
+		lastUpdate: BigNumberish,
+		lastBalance: BigNumberish,
+		currentReceivers: DripsReceiverStruct[],
+		balanceDelta: BigNumberish,
+		newReceivers: DripsReceiverStruct[]
+	) {
+		if (!this.signer) throw new Error('Not connected to wallet');
 
-      validateDrips(newReceivers)
+		validateDrips(newReceivers);
 
-      const contractSigner = this.hubContract.connect(this.signer)
+		const contractSigner = this.hubContract.connect(this.signer);
 
-      return contractSigner['setDrips(uint64,uint128,(address,uint128)[],int128,(address,uint128)[])'](
-        lastUpdate, lastBalance, currentReceivers, balanceDelta, newReceivers
-      )
-    }
+		return contractSigner['setDrips(uint64,uint128,(address,uint128)[],int128,(address,uint128)[])'](
+			lastUpdate,
+			lastBalance,
+			currentReceivers,
+			balanceDelta,
+			newReceivers
+		);
+	}
 
-  updateSubAccountDrips: DaiDripsHub['setDrips(uint256,uint64,uint128,(address,uint128)[],int128,(address,uint128)[])'] =
-    (subAccountId, lastUpdate, lastBalance, currentReceivers, balanceDelta, newReceivers) => {
-      if (!this.signer) throw new Error("Not connected to wallet")
+	updateSubAccountDrips(
+		subAccountId: BigNumberish,
+		lastUpdate: BigNumberish,
+		lastBalance: BigNumberish,
+		currentReceivers: DripsReceiverStruct[],
+		balanceDelta: BigNumberish,
+		newReceivers: DripsReceiverStruct[]
+	) {
+		if (!this.signer) throw new Error('Not connected to wallet');
 
-      validateDrips(newReceivers)
+		validateDrips(newReceivers);
 
-      const contractSigner = this.hubContract.connect(this.signer)
+		const contractSigner = this.hubContract.connect(this.signer);
 
-      return contractSigner['setDrips(uint256,uint64,uint128,(address,uint128)[],int128,(address,uint128)[])'](
-        subAccountId, lastUpdate, lastBalance, currentReceivers, balanceDelta, newReceivers
-      )
-    }
+		return contractSigner['setDrips(uint256,uint64,uint128,(address,uint128)[],int128,(address,uint128)[])'](
+			subAccountId,
+			lastUpdate,
+			lastBalance,
+			currentReceivers,
+			balanceDelta,
+			newReceivers
+		);
+	}
 
-  updateUserSplits: DaiDripsHub['setSplits'] =
-    (currentReceivers, newReceivers) => {
-      if (!this.signer) throw new Error("Not connected to wallet")
+	updateUserSplits(currentReceivers: SplitsReceiverStruct[], newReceivers: SplitsReceiverStruct[]) {
+		if (!this.signer) throw new Error('Not connected to wallet');
 
-      validateSplits(newReceivers)
+		validateSplits(newReceivers);
 
-      const contractSigner = this.hubContract.connect(this.signer)
+		const contractSigner = this.hubContract.connect(this.signer);
 
-      return contractSigner['setSplits'](currentReceivers, newReceivers)
-    }
+		return contractSigner.setSplits(currentReceivers, newReceivers);
+	}
 
 	giveFromUser(receiver: string, amount: BigNumberish) {
-		if (!this.signer) throw new Error("Not connected to wallet")
-		if (!utils.isAddress(receiver)) throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`)
+		if (!this.signer) throw new Error('Not connected to wallet');
+		if (!utils.isAddress(receiver)) throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`);
 
-		const contractSigner = this.hubContract.connect(this.signer)
+		const contractSigner = this.hubContract.connect(this.signer);
 
-		return contractSigner["give(address,uint128)"](receiver, amount)
+		return contractSigner['give(address,uint128)'](receiver, amount);
 	}
 
 	giveFromAccount(account: BigNumberish, receiver: string, amount: BigNumberish) {
-		if (!this.signer) throw new Error("Not connected to wallet")
-		if (!utils.isAddress(receiver)) throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`)
+		if (!this.signer) throw new Error('Not connected to wallet');
+		if (!utils.isAddress(receiver)) throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`);
 
-		const contractSigner = this.hubContract.connect(this.signer)
+		const contractSigner = this.hubContract.connect(this.signer);
 
-		return contractSigner["give(uint256,address,uint128)"](account, receiver, amount)
+		return contractSigner['give(uint256,address,uint128)'](account, receiver, amount);
 	}
 
-  // check how much DAI the DripsHub is allowed to spend on behalf of the signed-in user
-  getAllowance() {
-    if (!this.address) throw new Error("Must call connect() before calling getAllowance()")
+	// check how much DAI the DripsHub is allowed to spend on behalf of the signed-in user
+	getAllowance() {
+		if (!this.address) throw new Error('Must call connect() before calling getAllowance()');
 
-    return this.daiContract.allowance(this.address, this.contractDetails.CONTRACT_DRIPS_HUB);
-  }
+		return this.daiContract.allowance(this.address, this.contractDetails.CONTRACT_DRIPS_HUB);
+	}
 
-  getAmountCollectableWithSplits: DaiDripsHub['collectable'] =
-    (address, currentSplits) => {
-      if (!this.provider) throw new Error("Must have a provider defined to query the collectable balance")
+	getAmountCollectableWithSplits: DaiDripsHub['collectable'] = (address, currentSplits) => {
+		if (!this.provider) throw new Error('Must have a provider defined to query the collectable balance');
 
-      return this.hubContract.collectable(address, currentSplits)
-    }
-
+		return this.hubContract.collectable(address, currentSplits);
+	};
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,130 +1,130 @@
 import type { BigNumberish, Event } from 'ethers';
-import type { DripsReceiverStruct, SplitsReceiverStruct } from '../contracts/DaiDripsHub';
 import { BigNumber, utils, constants } from 'ethers';
+import type { DripsReceiverStruct, SplitsReceiverStruct } from '../contracts/DaiDripsHub';
 import { DripsConfig } from './subgraph';
 
-export const oneMonth = 30 * 24 * 60 * 60
-export const TOTAL_SPLITS_WEIGHT = 1000000
+export const oneMonth = 30 * 24 * 60 * 60;
+export const TOTAL_SPLITS_WEIGHT = 1000000;
 
 export const fromWei = (wei: BigNumberish) => {
-  wei = BigNumber.isBigNumber(wei) ? wei : BigNumber.from(wei)
-  return wei.div(constants.WeiPerEther)
-}
-
-export const toWei = (dai: BigNumberish) => {
-  return utils.parseUnits(dai.toString())
-}
-
-export const toDAI = (wei: BigNumberish, frmt = 'pretty', roundTo: number) => {
-  const dai = utils.formatEther(wei)
-  if (frmt === 'exact') {
-    return dai
-  }
-  return round(parseFloat(dai), roundTo)
-}
+	const weiAsBigNum = BigNumber.isBigNumber(wei) ? wei : BigNumber.from(wei);
+	return weiAsBigNum.div(constants.WeiPerEther);
+};
 
 // clip to nearest hundredth (dont round up)
-export const round = (num, dec = 2) => (Math.floor(num * 100) / 100).toFixed(dec)
+export const round = (num: number, dec = 2) => (Math.floor(num * 100) / 100).toFixed(dec);
+
+export const toWei = (dai: BigNumberish) => utils.parseUnits(dai.toString());
+
+export const toDAI = (wei: BigNumberish, roundTo: number, format = 'pretty') => {
+	const dai = utils.formatEther(wei);
+	if (format === 'exact') {
+		return dai;
+	}
+	return round(parseFloat(dai), roundTo);
+};
 
 export const toDAIPerMo = (wei: BigNumberish) => {
-  wei = BigNumber.isBigNumber(wei) ? wei : BigNumber.from(wei)
-  const dai = utils.formatEther(wei.mul(oneMonth))
-  
-  // round to nearest hundredth
-  return Math.round(parseFloat(dai) * 100) / 100
-}
+	const weiAsBigNum = BigNumber.isBigNumber(wei) ? wei : BigNumber.from(wei);
+	const dai = utils.formatEther(weiAsBigNum.mul(oneMonth));
 
-export const toWeiPerSec = (dai: BigNumberish = 0) => {
-  // warning! BN will clip off the decimal...
-  // (but maybe good for when setting minAmtPerSec)
-  return utils.parseUnits(dai.toString()).div(oneMonth)
-}
+	// round to nearest hundredth
+	return Math.round(parseFloat(dai) * 100) / 100;
+};
 
+export const toWeiPerSec = (dai: BigNumberish = 0) =>
+	// warning! BN will clip off the decimal...
+	// (but maybe good for when setting minAmtPerSec)
+	utils.parseUnits(dai.toString()).div(oneMonth);
 
 export type PercentageSplit = {
-  address: string,
-  percent: number
-}
+	address: string;
+	percent: number;
+};
 
 /*
-* Formats splits for contract method input.
-*/
+ * Formats splits for contract method input.
+ */
 export const formatSplits = (splits: PercentageSplit[] = []): SplitsReceiverStruct[] => {
-  const validSplits = splits.filter(s => s.address.length && s.percent > 0);
-  return validSplits
-    .map(s => ({
-      receiver: s.address.toLowerCase(),
-      weight: s.percent / 100 * TOTAL_SPLITS_WEIGHT
-    }))
-    .sort((a, b) => a < b ? -1 : 1);
+	const validSplits = splits.filter((s) => s.address.length && s.percent > 0);
+	return validSplits
+		.map((s) => ({
+			receiver: s.address.toLowerCase(),
+			weight: (s.percent / 100) * TOTAL_SPLITS_WEIGHT
+		}))
+		.sort((a, b) => (a < b ? -1 : 1));
+};
+
+export function validateDrips(drips: DripsReceiverStruct[]) {
+	if (!drips?.length) {
+		return;
+	}
+
+	for (let i = 0; i < drips.length; i++) {
+		const { receiver } = drips[i];
+		if (!utils.isAddress(receiver)) {
+			throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`);
+		}
+	}
 }
 
-export function validateDrips (drips: DripsReceiverStruct[]) {
-  drips = drips || [];
-  for (let i = 0; i < drips.length; i++) {
-    const {receiver} = drips[i];
-    if (!utils.isAddress(receiver)) {
-      throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`)
-    }
-  }
-}
+export function validateSplits(splits: SplitsReceiverStruct[]) {
+	if (!splits?.length) {
+		return;
+	}
 
-export function validateSplits (splits: SplitsReceiverStruct[]) {
-  splits = splits || []
-  for (let i = 0; i < splits.length; i++) {
-    const { receiver } = splits[i]
-    if (!utils.isAddress(receiver)) {
-      throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`)
-    }
-  }
+	for (let i = 0; i < splits.length; i++) {
+		const { receiver } = splits[i];
+		if (!utils.isAddress(receiver)) {
+			throw new Error(`Invalid recipient: "${receiver}" is not an Ethereum address`);
+		}
+	}
 }
 
 export const getDripsWithdrawable = (config: DripsConfig) => {
-  // https://discord.com/channels/841318878125490186/875668327614255164/918094059732623411
-  // - Look at the latest user's DripsUpdated, it has a timestamp, uint128 balance and DripsReceiver[] receivers
-  // - Add up all the receiers' amtPerSec, it's totalAmtPerSec
-  // - withdrawable = eventBalance - (currTimestamp - eventTimestamp) * totalAmtPerSec
-  // - if withdrawable < 0, withdrawable = eventBalance % totalAmtPerSec
-  try {
-    const currTimestamp = Math.floor(new Date().getTime() / 1000) // sec
-    const totalAmtPerSec = config.receivers.reduce((acc, curr) => acc.add(curr.amtPerSec), BigNumber.from(0))
-    const eventBalance = BigNumber.from(config.balance)
-    let withdrawable = eventBalance.sub(totalAmtPerSec.mul(currTimestamp - parseInt(config.timestamp)))
-    if (withdrawable.lt(0)) {
-      withdrawable = eventBalance.mod(totalAmtPerSec)
-    }
-    return withdrawable
-  } catch (e) {
-    console.error(e)
-    return null
-  }
-}
+	// https://discord.com/channels/841318878125490186/875668327614255164/918094059732623411
+	// - Look at the latest user's DripsUpdated, it has a timestamp, uint128 balance and DripsReceiver[] receivers
+	// - Add up all the receiers' amtPerSec, it's totalAmtPerSec
+	// - withdrawable = eventBalance - (currTimestamp - eventTimestamp) * totalAmtPerSec
+	// - if withdrawable < 0, withdrawable = eventBalance % totalAmtPerSec
+	try {
+		const currTimestamp = Math.floor(new Date().getTime() / 1000); // sec
+		const totalAmtPerSec = config.receivers.reduce((acc, curr) => acc.add(curr.amtPerSec), BigNumber.from(0));
+		const eventBalance = BigNumber.from(config.balance);
+		let withdrawable = eventBalance.sub(totalAmtPerSec.mul(currTimestamp - parseInt(config.timestamp, 10)));
+		if (withdrawable.lt(0)) {
+			withdrawable = eventBalance.mod(totalAmtPerSec);
+		}
+		return withdrawable;
+	} catch (e) {
+		return null;
+	}
+};
 
 export const getDripsWithdrawableFromEvent = async (event: Event) => {
-  // https://discord.com/channels/841318878125490186/875668327614255164/918094059732623411
-  // - Look at the latest user's DripsUpdated, it has a timestamp, uint128 balance and DripsReceiver[] receivers
-  // - Add up all the receiers' amtPerSec, it's totalAmtPerSec
-  // - withdrawable = eventBalance - (currTimestamp - eventTimestamp) * totalAmtPerSec
-  // - if withdrawable < 0, withdrawable = eventBalance % totalAmtPerSec
-  try {
-    const currTimestamp = Math.floor(new Date().getTime() / 1000) // sec
-    const eventTimestamp = (await event.getBlock()).timestamp // sec
-    const receivers = event.args[2] as [number,number][] // TODO (typing);
-    const totalAmtPerSec = receivers.reduce<BigNumber>((acc, curr) => acc.add(curr[1]), BigNumber.from(0))
-    const eventBalance = event.args[1] as BigNumber;
-    let withdrawable = eventBalance.sub(totalAmtPerSec.mul(currTimestamp - eventTimestamp))
-    if (withdrawable.lt(0)) {
-      withdrawable = eventBalance.mod(totalAmtPerSec)
-    }
-    return withdrawable
-  } catch (e) {
-    console.error(e)
-    return null
-  }
-}
+	// https://discord.com/channels/841318878125490186/875668327614255164/918094059732623411
+	// - Look at the latest user's DripsUpdated, it has a timestamp, uint128 balance and DripsReceiver[] receivers
+	// - Add up all the receiers' amtPerSec, it's totalAmtPerSec
+	// - withdrawable = eventBalance - (currTimestamp - eventTimestamp) * totalAmtPerSec
+	// - if withdrawable < 0, withdrawable = eventBalance % totalAmtPerSec
+	try {
+		const currTimestamp = Math.floor(new Date().getTime() / 1000); // sec
+		const eventTimestamp = (await event.getBlock()).timestamp; // sec
+		const receivers = event.args[2] as [number, number][]; // TODO (typing);
+		const totalAmtPerSec = receivers.reduce<BigNumber>((acc, curr) => acc.add(curr[1]), BigNumber.from(0));
+		const eventBalance = event.args[1] as BigNumber;
+		let withdrawable = eventBalance.sub(totalAmtPerSec.mul(currTimestamp - eventTimestamp));
+		if (withdrawable.lt(0)) {
+			withdrawable = eventBalance.mod(totalAmtPerSec);
+		}
+		return withdrawable;
+	} catch (e) {
+		return null;
+	}
+};
 
 export const validateAddressInput = (input: string) => {
-  if (!utils.isAddress(input)) {
-    throw new Error(`"${input}" does not resolve to an Ethereum address`)
-  }
-}
+	if (!utils.isAddress(input)) {
+		throw new Error(`"${input}" does not resolve to an Ethereum address`);
+	}
+};
