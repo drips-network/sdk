718959019be0d02ca9dfd5909d88f94b52213b18
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2022-08-04 12:19:24 +0300

    refactor: change to named exports for DripsErrors

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export * from './utils';
 export * from './types';
 export * as gql from './gql';
 export * from './DripsSubgraph';
-export * as DripsError from './DripsError';
 export { default as DripsHubClient } from './DripsHubClient';
 export { default as AddressAppClient } from './AddressAppClient';
 export { default as DripsReceiverConfig } from './DripsReceiverConfig';
+export { DripsError, DripsErrors, DripsErrorCode } from './DripsError';
