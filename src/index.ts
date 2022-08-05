83224ef3a16953c63b23b7bcf4ce9efa9bd4f9cd
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2022-08-05 12:54:51 +0300

    refactor: export contract typings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 export * from './utils';
 export * from './types';
-export * from '../contracts';
 export * as gql from './gql';
 export * from './DripsSubgraph';
 export { default as DripsHubClient } from './DripsHubClient';
 export { default as AddressAppClient } from './AddressAppClient';
 export { default as DripsReceiverConfig } from './DripsReceiverConfig';
 export { DripsError, DripsErrors, DripsErrorCode } from './DripsError';
+
+// TODO: Contract typings. we can move these to separate package exports in TS 4.7 (https://github.com/microsoft/TypeScript/issues/33079)
+export * as AddressApp from '../contracts/AddressApp';
+export * as DripsHubLogic from '../contracts/DripsHubLogic';
