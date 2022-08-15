d1917b8929a114b9f8a8fcd4ab1072193f76ea80
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2022-08-15 14:33:34 +0300

    refactor: update graphql queries

--- src/gql.ts
+++ src/gql.ts
@@ -1,6 +1,10 @@
 export const getUserAssetConfigs = `#graphql
 query getUserAssetConfigs($userId: ID!) {
   user(id: $userId) {
+		splitsEntries {
+			receiverUserId
+			weight
+		}
     assetConfigs {
       id
       dripsEntries {
@@ -24,6 +28,10 @@ export const getUserAssetConfigById = `#graphql
 query getUserAssetConfigById($configId: ID!) {
 	userAssetConfig(id: $configId) {
 		id
+		splitsEntries {
+			receiverUserId
+			weight
+		}
 		dripsEntries {
 			id
 			receiverUserId
