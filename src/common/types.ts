3dd695b526ee84fa1af39cf80d3bcf9ef7ecda91
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2022-09-08 12:19:42 +0200

    refactor(drips config): change property types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,10 +1,10 @@
-import type { BigNumberish } from 'ethers';
+import type { BigNumber } from 'ethers';
 
 export type DripsReceiverConfig = {
 	/** The UNIX timestamp when dripping should start. Must be greater than or equal to `0`. If set to `0`, the smart contract will use the timestamp when drips are configured. */
-	readonly start: BigNumberish;
+	readonly start: BigNumber;
 	/** The duration (in seconds) of dripping. Must be greater than or equal to `0`. If set to `0`, the smart contract will drip until the balance runs out. */
-	readonly duration: BigNumberish;
+	readonly duration: BigNumber;
 	/** The amount per second being dripped. Must be greater than `0`. */
-	readonly amountPerSec: BigNumberish;
+	readonly amountPerSec: BigNumber;
 };
