5bdbc10350297ea19d4d39c68913e45216580f2b
Morgan Brown <git@email.mhgbrown.is>
2025-10-20 15:58:42 +0200

    Flesh out a full repoDriverAccountMetadataSchemaV6

--- src/internal/metadata/schemas/index.ts
+++ src/internal/metadata/schemas/index.ts
@@ -13,6 +13,7 @@ import {repoDriverAccountMetadataSchemaV3} from './repo-driver/v3';
 import {repoDriverAccountMetadataSchemaV4} from './repo-driver/v4';
 import {repoDriverAccountMetadataSchemaV5} from './repo-driver/v5';
 import { nftDriverAccountMetadataSchemaV7 } from './nft-driver/v7';
+import { repoDriverAccountMetadataSchemaV6 } from './repo-driver/v6';
 
 export const nftDriverAccountMetadataParser = createVersionedParser([
   nftDriverAccountMetadataSchemaV7.parse,
@@ -29,6 +30,7 @@ export const addressDriverAccountMetadataParser = createVersionedParser([
 ]);
 
 export const repoDriverAccountMetadataParser = createVersionedParser([
+  repoDriverAccountMetadataSchemaV6.parse,
   repoDriverAccountMetadataSchemaV5.parse,
   repoDriverAccountMetadataSchemaV4.parse,
   repoDriverAccountMetadataSchemaV3.parse,
--- src/internal/metadata/schemas/repo-driver/v6.ts
+++ src/internal/metadata/schemas/repo-driver/v6.ts
@@ -1,4 +1,10 @@
 import z from "zod";
+import {repoDriverAccountMetadataSchemaV5} from './v5';
+import {
+  addressDriverSplitReceiverSchema,
+  repoDriverSplitReceiverSchema,
+} from './v2';
+import { dripListSplitReceiverSchema } from "./v3";
 
 export const orcidSplitReceiverSchema = z.object({
   type: z.literal('orcid'),
@@ -6,3 +12,20 @@ export const orcidSplitReceiverSchema = z.object({
   accountId: z.string(),
   orcidId: z.string()
 });
+
+const repoDriverAccountSplitsSchema = z.object({
+  maintainers: z.array(addressDriverSplitReceiverSchema),
+  dependencies: z.array(
+    z.union([
+      dripListSplitReceiverSchema,
+      repoDriverSplitReceiverSchema,
+      addressDriverSplitReceiverSchema,
+      orcidSplitReceiverSchema,
+    ]),
+  ),
+});
+
+export const repoDriverAccountMetadataSchemaV6 =
+  repoDriverAccountMetadataSchemaV5.extend({
+    splits: repoDriverAccountSplitsSchema,
+  });
